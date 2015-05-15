1b09c2f349ca7b0ff7b8c445ef9174a8d11ca92d
Samuel Skanberg <samuel.skanberg@gmail.com>
2015-05-15 20:00:54 +0200

    Read file and find the least edit distance

--- set1/challenge6_break_repeating_xor.py
+++ set1/challenge6_break_repeating_xor.py
@@ -1,8 +1,16 @@
 from challenge2_xor import byteArrayXOR
+from base64 import *
+import operator
 
 def stringEditDistance(str1, str2):
+    assert(len(str1) == len(str2))
+
     b1 = bytearray(str1)
     b2 = bytearray(str2)
+
+    return byteArrayEditDistance(b1, b2)
+    
+def byteArrayEditDistance(b1, b2):
 
     res = byteArrayXOR(b1, b2)
     distance = 0
@@ -12,11 +20,37 @@ def stringEditDistance(str1, str2):
     return distance
 
 
-
 if __name__ == "__main__":
     str1 = "this is a test"
     str2 = "wokka wokka!!!"
 
-    editDistance = stringEditDistance(str1, str2)
-    assert(editDistance == 37)
-    print editDistance
+    editDistanceTest = stringEditDistance(str1, str2)
+    assert(editDistanceTest == 37)
+    print editDistanceTest
+
+
+    f = open('6.txt', 'r')
+
+    fileContent = ""
+    for b64line in f:
+        b64line = b64line.rstrip()
+        fileContent += b64line
+    f.close()
+
+    print "WHole content:"
+    print fileContent
+    fileContentBytes = b64decode(fileContent)
+   
+    keySizeWithEditDistance = {} 
+    for keySize in range(2, 41):
+        part1 = fileContentBytes[0:keySize]
+        part2 = fileContentBytes[keySize:keySize+keySize]
+        #print "Keysize: "+str(keySize)
+
+        editDistance = stringEditDistance(part1, part2)
+        normalizedEditDistance = editDistance*1.0/keySize
+        keySizeWithEditDistance[keySize] = normalizedEditDistance
+        #print "Normalized edit distance: "+str(normalizedEditDistance)
+
+    print keySizeWithEditDistance
+    print min(keySizeWithEditDistance.iteritems(), key=operator.itemgetter(1))
